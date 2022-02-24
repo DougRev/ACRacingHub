34e313a051b4a12757ec0e0b22dca76ec68e5abf
Doug Revell <doug.revelljr@gmail.com>
2022-02-24 12:35:16 -0500

    Add Enrollment

---
+++ RacingHub.Data/Enrollment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RacingHub.Data
+{
+    public class Enrollment
+    {
+        [ForeignKey(nameof(Race))]
+        public int RaceId { get; set; }
+        public virtual Race Race { get; set; }
+
+        [ForeignKey(nameof(Race))]
+        public int TeamId { get; set; }
+        public virtual Team Team { get; set; }
+        public int UserId { get; set; }
+    }
+}
